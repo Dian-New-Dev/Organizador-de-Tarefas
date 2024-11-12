9a5b5b88d6095e8f23a91492b9924fdde3b6e864
Diego <diannewdev@gmail.com>
2024-11-11 20:49:13 -0400

    melhorias no style da pagina, footer adicionado

--- onload.js
+++ onload.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function (){
     const storedTasks = JSON.parse(localStorage.getItem('tasks-test-2')) || [];
+    console.log(storedTasks)
     gerarPainelDeTarefas(storedTasks);
     // inserirNovaTarefaCumprida();
 })
